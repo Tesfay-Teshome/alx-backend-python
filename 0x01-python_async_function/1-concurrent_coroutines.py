16673e764148e713d4d1297de2d49e1a8b1dccc4
Tesfay-Teshome <tesfix2017@gmail.com>
2023-02-27 10:21:23 +0300

    Unittest and Integration tests readme file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    spawn wait_random n times with the specified max_delay.
+    wait_random n times with the specified max_delay.
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
